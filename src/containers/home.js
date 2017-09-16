337301d00bf6dad67a09b1e6e84f533d19a1bd10
awaisabir <awaisabirqureshi@gmail.com>
2017-09-16 16:34:20 -0400

    Fixed styling issue

--- src/containers/home.js
+++ src/containers/home.js
@@ -17,57 +17,21 @@ class Home extends Component{
 
   render(){
     return(
-      <iframe scrolling='no' className='iframeChat'
-      src='https://webchat.botframework.com/embed/therapybot-hackthenorth?s=iZnsMGzDq9g.cwA.YGA.n1YcNdK39lS7Tn6n3qF-ZMR0YKEj6quUUqnGlTSWruI'> </iframe>
+      <div>
+        <iframe style={{width: '100%', height: '100vh', marginTop: '-1px'}} scrolling='no' className='iframeChat'
+        src='https://webchat.botframework.com/embed/therapybot-hackthenorth?s=iZnsMGzDq9g.cwA.YGA.n1YcNdK39lS7Tn6n3qF-ZMR0YKEj6quUUqnGlTSWruI'>
+        </iframe>
+      </div>
     );
   }
 }
-const style = {
-  menuItem:{
-    color:'#fffce1',
-    textDecoration: 'none',
-  },
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '36px',
-    top: '36px'
-  },
-  bmBurgerBars: {
-    background: '#373a47'
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px'
-  },
-  bmCross: {
-    background: '#bdc3c7'
-  },
-  bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em'
-  },
-  bmMorphShape: {
-    fill: '#373a47'
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em'
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)'
-  },
-  links:{
-    color:"#fff"
-  },
-}
+
 function mapStateToProps(state) {
     return {
         users: state.users
     };
 }
+
 function matchDispatchToProps(dispatch){
     return bindActionCreators({selectUser: selectUser}, dispatch);
 }
