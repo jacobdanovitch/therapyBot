968c985e9f912b3be7aed3c1fec3a522d9556589
percyteng <percytsy@gmail.com>
2017-09-16 17:43:25 -0400

    got voice

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,18 +1,39 @@
 import React, {Component} from 'react';
 import {Link} from "react-router";
+import { ReactMic } from 'react-mic';
 
 
 export default class First extends Component{
   constructor(){
     super()
     this.state={
-
+      record: false,
+      buttonTxt: 'Start'
     }
+  }
+
+  onStop(recordedBlob){
+    console.log('recordedBlob is: ', recordedBlob);
   }
   render(){
     return(
-        <iframe scrolling='no' className='iframeChat'
-        src='https://webchat.botframework.com/embed/therapybot-hackthenorth?s=iZnsMGzDq9g.cwA.YGA.n1YcNdK39lS7Tn6n3qF-ZMR0YKEj6quUUqnGlTSWruI'> </iframe>
+      <div className='container'>
+        <ReactMic
+          record={this.state.record}
+          className="sound-wave"
+          onStop={this.onStop.bind(this)}
+          strokeColor="#000000"
+          backgroundColor="#FF4081" />
+        <div className='mic' onClick={()=>{
+          this.setState({record:!this.state.record})
+          if (this.state.buttonTxt==='Start')
+            this.setState({buttonTxt:'Stop'})
+          else
+            this.setState({buttonTxt:'Start'})
+        }}/>
+        <p>{this.state.buttonTxt}</p>
+      </div>
+
     );
   }
 }
--- src/containers/home.js
+++ src/containers/home.js
@@ -3,9 +3,6 @@ import {Link} from "react-router";
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux'
 import {selectUser} from '../actions/index'
-import UserDetails from './user-detail';
-import Menu from 'react-motion-menu'
-var BurgerMenu = require('react-burger-menu').pushRotate;
 
 class Home extends Component{
   constructor(props){
@@ -18,9 +15,7 @@ class Home extends Component{
   render(){
     return(
       <div>
-        <iframe style={{maxWidth: '100%', height: '100vh', marginTop: '-1px'}} scrolling='no' className='iframeChat'
-        src='https://webchat.botframework.com/embed/therapybot-hackthenorth?s=iZnsMGzDq9g.cwA.YGA.n1YcNdK39lS7Tn6n3qF-ZMR0YKEj6quUUqnGlTSWruI'>
-        </iframe>
+        {this.props.children}
       </div>
     );
   }
